c08a5be420cfede44730f4c6ed476e2491c084d9
CESDE <CESDE@PC-04>
2018-10-10 08:46:53 -0500

    Generando mis Beans

--- AplicacionPrueba/src/Beans/Persona.java
+++ AplicacionPrueba/src/Beans/Persona.java
@@ -10,5 +10,66 @@ package Beans;
  * @author CESDE
  */
 public class Persona {
+    private int id_persona;
+    private String nombre;
+    private String ape_paterno;
+    private String Ape_materno;
+
+    /**
+     * @return the id_persona
+     */
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    /**
+     * @param id_persona the id_persona to set
+     */
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the ape_paterno
+     */
+    public String getApe_paterno() {
+        return ape_paterno;
+    }
+
+    /**
+     * @param ape_paterno the ape_paterno to set
+     */
+    public void setApe_paterno(String ape_paterno) {
+        this.ape_paterno = ape_paterno;
+    }
+
+    /**
+     * @return the Ape_materno
+     */
+    public String getApe_materno() {
+        return Ape_materno;
+    }
+
+    /**
+     * @param Ape_materno the Ape_materno to set
+     */
+    public void setApe_materno(String Ape_materno) {
+        this.Ape_materno = Ape_materno;
+    }
     
+            
 }
